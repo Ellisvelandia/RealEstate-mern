ed523ed0d92a4d27e2c18d918eaf3808f70b8fa1
Ellisvelandia <eyis619l@gmail.com>
2023-03-16 19:42:31 -0500

    authController

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
+const authController = require("./controllers/authController");
 const app = express();
 
 require("./connect/db");
-
 mongoose.set("strictQuery", false);
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/auth", authController);
 
 
-
-
-app.listen(process.env.PORT, () =>
-  console.log("Server has been started successfully")
-);
+app.listen(4001, () => {
+  console.log(`Node API app is running on port 4001`);
+});
