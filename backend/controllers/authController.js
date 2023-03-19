3da85e6db8df72c14c04bdbc8b8422a25ace3cbb
Ellisvelandia <eyis619l@gmail.com>
2023-03-19 12:46:09 -0500

    properties

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,8 +6,11 @@ const jwt = require("jsonwebtoken");
 authController.post("/register", async (req, res) => {
   try {
     const isExisting = await User.findOne({ email: req.body.email });
+
     if (isExisting) {
-      throw new Error("Already such an email registered!");
+      return res
+        .status(500)
+        .json({ msg: "Email is already taken by another user." });
     }
 
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -32,19 +35,20 @@ authController.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
-      throw new Error("Wrong credentials!");
+      return res.status(500).json({ msg: "Wrong credentials. Try again!" });
     }
 
     const comparePass = await bcrypt.compare(req.body.password, user.password);
     if (!comparePass) {
-      throw new Error("Wrong credentials");
+      return res.status(500).json({ msg: "Wrong credentials. Try again!" });
     }
 
+    const { password, ...others } = user._doc;
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "4h",
     });
-    const { password, ...others } = user._doc;
 
+    console.log({ others, token });
     return res.status(200).json({ others, token });
   } catch (error) {
     return res.status(500).json(error.message);
--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -1,17 +1,22 @@
-const Property = require("../models/Property");
-const propertyController = require("express").Router();
 const verifyToken = require("../middlewares/verifyToken");
+const Property = require("../models/Property");
+const User = require("../models/User");
+const propertyController = require("express").Router();
 
+// get all
 propertyController.get("/getAll", async (req, res) => {
   try {
     const properties = await Property.find({});
 
+    console.log(properties);
+
     return res.status(200).json(properties);
   } catch (error) {
-    return res.status(500).json(error.message);
+    console.error(error);
   }
 });
 
+// get featured
 propertyController.get("/find/featured", async (req, res) => {
   try {
     const featuredProperties = await Property.find({ featured: true }).populate(
@@ -20,59 +25,61 @@ propertyController.get("/find/featured", async (req, res) => {
     );
     return res.status(200).json(featuredProperties);
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(500).json(error);
   }
 });
 
+// get all from type
 propertyController.get("/find", async (req, res) => {
   const type = req.query;
+  let properties = [];
   try {
     if (type) {
-      const properties = await Property.find(type).populate(
-        "currentOwner",
-        "-password"
-      );
-      return res.status(200).json(properties);
+      properties = await Property.find(type).populate("owner", "-password");
     } else {
-      return res.status(500).json({ msg: "No such type" });
+      properties = await Property.find({});
     }
+
+    return res.status(200).json(properties);
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(500).json(error);
   }
 });
 
+// TODO FETCH TYPE OF PROPERTIES. EX: {BEACH: 34, MOUNTAIN: 23}
 propertyController.get("/find/types", async (req, res) => {
   try {
     const beachType = await Property.countDocuments({ type: "beach" });
     const mountainType = await Property.countDocuments({ type: "mountain" });
     const villageType = await Property.countDocuments({ type: "village" });
 
-    return res.status(200).json({
-      beach: beachType,
-      mountain: mountainType,
-      village: villageType,
-    });
+    return res
+      .status(200)
+      .json({ beach: beachType, mountain: mountainType, village: villageType });
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(500).json(error);
   }
 });
 
+// TODO FETCH INDIVIDUAL PROPERTY
 propertyController.get("/find/:id", async (req, res) => {
   try {
-    const property = await Property.find(req.params.id).populate(
+    const property = await Property.findById(req.params.id).populate(
       "currentOwner",
       "-password"
     );
+
     if (!property) {
-      throw new Error("no such property with this id");
+      throw new Error("No such property with that id");
     } else {
       return res.status(200).json(property);
     }
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(500).json(error);
   }
 });
 
+// create estate
 propertyController.post("/", verifyToken, async (req, res) => {
   try {
     const newProperty = await Property.create({
@@ -82,44 +89,43 @@ propertyController.post("/", verifyToken, async (req, res) => {
 
     return res.status(201).json(newProperty);
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(500).json(error);
   }
 });
 
+// update estate
 propertyController.put("/:id", verifyToken, async (req, res) => {
   try {
     const property = await Property.findById(req.params.id);
-    if (property.currentOwner !== req.user.id) {
+    if (property.owner !== req.user.id) {
       throw new Error("You are not allowed to update other people properties");
-    } else {
-      const updateProperty = await Property.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
     }
 
-    return res.status(200).json(updateProperty);
+    const updatedProperty = await Property.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    return res.status(200).json(updatedProperty);
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(500).json(error);
   }
 });
 
-propertyController.delete("/:id", verifyToken, async (req, res) => {
+// delete estate
+propertyController.put("/:id", verifyToken, async (req, res) => {
   try {
     const property = await Property.findById(req.params.id);
-
-    if (property.currentOwner !== req.user.id) {
-      throw new Error("Your are not allowed to delete toher people properties");
-    } else {
-      await property.delete();
-
-      return res.status(200).json({ msg: "Successfully deleted property" });
+    if (property.owner !== req.user.id) {
+      throw new Error("You are not allowed to delete other people properties");
     }
+
+    await property.delete();
+
+    return res.status(200).json({ msg: "Successfully deleted property" });
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(500).json(error);
   }
 });
 
